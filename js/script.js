2affa457c6c166ed6b12a60158565e048bd36dd7
Michał Kucharski <31617174+BigRedMnstr@users.noreply.github.com>
2017-12-05 16:08:03 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 function loadData() {
 
     var $body = $('body');
@@ -17,30 +16,38 @@ function loadData() {
 
     $greeting.text('So, you want to live at ' + address + '?');
 
+    // load streetview
     var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
- 
+
+
+
+
+    // load nytimes
+    // obviously, replace all the "X"s with your own API key
     var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=ab07736ad38e47f8b481257ba25cb929';
-    $.getJSON(nytimesUrl, function (data) {
+    $.getJSON(nytimesUrl, function(data){
 
         $nytHeaderElem.text('New York Times Articles About ' + cityStr);
 
         articles = data.response.docs;
         for (var i = 0; i < articles.length; i++) {
             var article = articles[i];
-            $nytElem.append('<li class="article">' +
-                    '<a href="' + article.web_url + '">' + article.headline.main + '</a>' +
-                    '<p>' + article.snippet + '</p>' +
-                    '</li>');
-        }
-        ;
+            $nytElem.append('<li class="article">'+
+                '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
+                '<p>' + article.snippet + '</p>'+
+            '</li>');
+        };
 
-    }).error(function (e) {
+    }).error(function(e){
         $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
     });
 
+
+
+    // load wikipedia data
     var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
-    var wikiRequestTimeout = setTimeout(function () {
+    var wikiRequestTimeout = setTimeout(function(){
         $wikiElem.text("failed to get wikipedia resources");
     }, 8000);
 
@@ -48,23 +55,20 @@ function loadData() {
         url: wikiUrl,
         dataType: "jsonp",
         jsonp: "callback",
-        success: function (response) {
+        success: function( response ) {
             var articleList = response[1];
 
             for (var i = 0; i < articleList.length; i++) {
                 articleStr = articleList[i];
                 var url = 'http://en.wikipedia.org/wiki/' + articleStr;
                 $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
-            }
-            ;
+            };
 
             clearTimeout(wikiRequestTimeout);
         }
     });
 
-
     return false;
-}
-;
+};
 
 $('#form-container').submit(loadData);
