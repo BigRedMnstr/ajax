bd59738ddfe49e68a27b8a6306e3795ed21465e7
Michał Kucharski <31617174+BigRedMnstr@users.noreply.github.com>
2017-12-05 16:35:21 +0100

    defined vars

--- js/script.js
+++ js/script.js
@@ -24,22 +24,24 @@ function loadData() {
 
 
     // load nytimes
-    // obviously, replace all the "X"s with your own API key
+    
     var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=ab07736ad38e47f8b481257ba25cb929';
-    $.getJSON(nytimesUrl, function(data){
+    $.getJSON(nytimesUrl, function (data) {
 
         $nytHeaderElem.text('New York Times Articles About ' + cityStr);
 
+        var articles;
         articles = data.response.docs;
         for (var i = 0; i < articles.length; i++) {
             var article = articles[i];
-            $nytElem.append('<li class="article">'+
-                '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
-                '<p>' + article.snippet + '</p>'+
-            '</li>');
-        };
+            $nytElem.append('<li class="article">' +
+                    '<a href="' + article.web_url + '">' + article.headline.main + '</a>' +
+                    '<p>' + article.snippet + '</p>' +
+                    '</li>');
+        }
+        ;
 
-    }).error(function(e){
+    }).error(function (e) {
         $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
     });
 
@@ -47,7 +49,7 @@ function loadData() {
 
     // load wikipedia data
     var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
-    var wikiRequestTimeout = setTimeout(function(){
+    var wikiRequestTimeout = setTimeout(function () {
         $wikiElem.text("failed to get wikipedia resources");
     }, 8000);
 
@@ -55,20 +57,23 @@ function loadData() {
         url: wikiUrl,
         dataType: "jsonp",
         jsonp: "callback",
-        success: function( response ) {
+        success: function (response) {
             var articleList = response[1];
+            var articleStr;
 
             for (var i = 0; i < articleList.length; i++) {
                 articleStr = articleList[i];
                 var url = 'http://en.wikipedia.org/wiki/' + articleStr;
                 $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
-            };
+            }
+            ;
 
             clearTimeout(wikiRequestTimeout);
         }
     });
 
     return false;
-};
+}
+;
 
 $('#form-container').submit(loadData);
