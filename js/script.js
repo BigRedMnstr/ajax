efe9995e83465365d7cda96b197e384066bee505
Michał Kucharski <31617174+BigRedMnstr@users.noreply.github.com>
2017-12-07 16:45:37 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,30 +1,41 @@
+
+var $body = $('body');
+var $wikiElem = $('#wikipedia-links');
+var $nytHeaderElem = $('#nytimes-header');
+var $nytElem = $('#nytimes-articles');
+var $greeting = $('#greeting');
+
+var streetStr;
+var cityStr;
+var address;
+
 function loadData() {
-
-    var $body = $('body');
-    var $wikiElem = $('#wikipedia-links');
-    var $nytHeaderElem = $('#nytimes-header');
-    var $nytElem = $('#nytimes-articles');
-    var $greeting = $('#greeting');
-
     // clear out old data before new request
     $wikiElem.text("");
     $nytElem.text("");
 
-    var streetStr = $('#street').val();
-    var cityStr = $('#city').val();
-    var address = streetStr + ', ' + cityStr;
-
     $greeting.text('So, you want to live at ' + address + '?');
 
-    // load streetview
+    streetStr = $('#street').val();
+    cityStr = $('#city').val();
+    address = streetStr + ', ' + cityStr;
+
+    streetView();
+    NYTimes();
+    Wikipedia();
+
+
+}
+;
+function streetView() {
     var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+}
+;
 
 
-
-
-    // load nytimes
-    
+// load nytimes
+function NYTimes() {
     var nytimesUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=ab07736ad38e47f8b481257ba25cb929';
     $.getJSON(nytimesUrl, function (data) {
 
@@ -44,10 +55,12 @@ function loadData() {
     }).error(function (e) {
         $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
     });
+}
+;
 
 
-
-    // load wikipedia data
+// load wikipedia data
+function Wikipedia() {
     var wikiUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
     var wikiRequestTimeout = setTimeout(function () {
         $wikiElem.text("failed to get wikipedia resources");
